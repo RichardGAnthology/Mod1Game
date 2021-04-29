41ab2767678338c3e9365c60516736919856ea36
Richard Gonzalez <gonzalezrmail@gmail.com>
2021-04-29 11:29:25 -0400

    removed api key

--- Mod 1 - The Game/scripts.js
+++ Mod 1 - The Game/scripts.js
@@ -1,7 +1,7 @@
 function getSymbols(){
     let firstSymb = document.getElementById("firstSymbol").value
     let secondSymb = document.getElementById("secondSymbol").value
-    return [secondSymb,firstSymb]
+    return [firstSymb,secondSymb]
 }
 
 let firstStock = 0
@@ -21,7 +21,7 @@ function getData(symbolsTest){
     let api = "https://api.twelvedata.com/time_series?"
     +"symbol="
     +symbolsTest
-    +",&interval=1min&outputsize=1&apikey=a18145a301e54e0bab5c05d5c419c6f4" 
+    +",&interval=1min&outputsize=1&apikey=yourAPIkey" 
     console.log(api)
     //searching the ulr's "database" with the parameters I set
     fetch(api) 
@@ -48,14 +48,6 @@ function onSubmit(){
     //the getData function runs on the symbols picked (fetched, formatted, etc)
     getData(symbolsTest)
     console.log("testingonSubmit")
-    
-    /*fetchInterval = setInterval(()=> {
-        getData(symbolsTest) 
-        fetchLimit++ 
-        if(fetchLimit==2){
-            clearInterval(fetchInterval)
-        }
-    },1000)*/
 }
 
 function displayData(data){
@@ -109,69 +101,7 @@ function displayData(data){
 }
 
 
-
-
 setInterval(() => {
     document.getElementById("barOne").style.width = parseInt(firstStock)+"px"
     document.getElementById("barTwo").style.width = parseInt(secondStock)+"px"
 }, 9000);
-
-
-// function Restart(){
-
-// }
-
-
-
-/*for(let playerOneScore=0;playerOneScore<=2;playerOneScore++){
-    if(PlayOnescore=2){
-        alert("Player One Won the game!")
-    }
-}
-*/
-
-//console.log(barOneTest)
-//console.log(barTwoTest)
-
-// document.addEventListener('DOMContentLoaded', () =>{
-//     const timeLeftDisplay = document.querySelector('#time-left')
-//     const startBtn = document.querySelector('#start-button')
-//     let timeLeft=15
-
-//     function countDown(){
-//         setInterval(function(){
-//             if (timeLeft<=0) {
-//                 clearInterval(timeLeft=0)
-//             }
-//             timeLeftDisplay.innerHTML=timeLeft
-//             timeLeft-=1
-//         }, 1000)
-//     }
-
-//     startBtn.addEventListener('click', countDown)
-// })
-
-
-/* //console.log(data[stock].values[0].open)
-document.getElementById("firstSymbol").addEventListener("click",getData);
-// document.getElementById("secondSymbol").addEventListener("click",getData);
-/for (const stock in stocks)
-    //{
-        console.log("testingloop")
-    //document.getElementById("div1").innerHTML += "<p>"+data[0].open+"</p>"
-   // };
-    console.log(data)
-
-  function changeSymbol(form){
-       // form.document.getElementById("firstSymbol").options["Pepsi"].value =
-       // form.document.getElementById("secondSymbol").options["Pespi"].value =
-    }
-
-    let api = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&"
- +"symbol="
- +symbol
- +"&interval=1min&outputsize=compact&apikey=3EODPZH75VPS0W1D"
-console.log(api)
-
-
-//let api = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&outputsize=full&apikey=3EODPZH75VPS0W1D"*/
